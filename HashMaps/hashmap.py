cbd1249e4fa24f7c4193017a065c5b13a686b264
Aashim Bajracharya <ashimbazracharya@gmail.com>
2021-02-04 18:49:53 +0545

    Test with setting values

--- HashMaps/hashmap.py
+++ HashMaps/hashmap.py
@@ -1,3 +1,4 @@
+import logging
 class HashTable:
 
     # Lets create empty list of given size
@@ -15,21 +16,29 @@ class HashTable:
         
         # Using hash method to generate index from the given key
         hashed_key = hash(key) % self.size
+        logging.warning(hashed_key)
 
         # Get the bucket corresponding to the generated index
         bucket = self.hash_table[hashed_key]
+        logging.warning(bucket)
 
+        gotten = False
         for index, rec in enumerate(bucket):
             rec_key, rec_value = rec
+            logging.warning("For loop !")
+            logging.warning(rec)
 
             # Check if the key to be inserted already exists in the bucket
             if rec_key == key:
+                logging.warning("Found !")
+                gotten = True
                 bucket[index] = (key, value)
                 return
-            else:
-                bucket.append((key, value))
-                return
 
+        if not gotten:
+            bucket.append((key, value))
+            logging.warning("Appending !")
+            return
     
     # Search the value and return it with specific key
     def get_value(self, key):
@@ -56,3 +65,22 @@ class HashTable:
             if rec_key == key:
                 bucket.pop[index]
                 return
+
+
+    # Print the items of the hash map
+    def __str__(self):
+        return "".join(str(hsh) for hsh in self.hash_table)
+
+
+# Get size for the hash
+hash_size = int(input("Size: "))
+
+# Get hash table
+hash_table = HashTable(hash_size)
+print(f"{hash_table}\n")
+
+# Get value and set in the hash table
+key_value = input("Key: ")
+content_value = input("Content: ")
+hash_table.set_value(key_value, content_value)
+print(f"{hash_table}\n")
