7f961f3d54c200b1a89a3b7ffc5405ae0eb9e28d
Aashim Bajracharya <ashimbazracharya@gmail.com>
2021-02-04 17:54:53 +0545

    Initiate with hashmaps
    - Create empty bucket list with given size

---
+++ HashMaps/hashmap.py
@@ -0,0 +1,10 @@
+class HashTable:
+
+    # Lets create empty list of given size
+
+    def __init__(self, size):
+        self.size = size
+        self.hash_table = self.create_buckets()
+
+    def create_buckets(self):
+        return [[] for _ in range(self.size)]
